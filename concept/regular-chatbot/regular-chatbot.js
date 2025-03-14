635007cdd423ef0899e422361d5dfee16ec930fb
Drahiri <kodzidg@o2.pl>
2025-03-14 14:21:36 +0100

    Regular Expressions

---
+++ concept/regular-chatbot/regular-chatbot.js
@@ -0,0 +1,65 @@
+// @ts-check
+
+/**
+ * Given a certain command, help the chatbot recognize whether the command is valid or not.
+ *
+ * @param {string} command
+ * @returns {boolean} whether or not is the command valid
+ */
+
+export function isValidCommand(command) {
+  let regex = /chatbot/i;
+  if (command.match(regex)?.index === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Given a certain message, help the chatbot get rid of all the emoji's encryption through the message.
+ *
+ * @param {string} message
+ * @returns {string} The message without the emojis encryption
+ */
+export function removeEmoji(message) {
+  let regex = new RegExp("emoji[0-9]*", "gi");
+  return message.replace(regex, "");
+}
+
+/**
+ * Given a certain phone number, help the chatbot recognize whether it is in the correct format.
+ *
+ * @param {string} number
+ * @returns {string} the Chatbot response to the phone Validation
+ */
+export function checkPhoneNumber(number) {
+  let regex = /\(\+[0-9]{2}\) [0-9]{3}-[0-9]{3}-[0-9]{3}/g;
+  if (regex.test(number)) {
+    return "Thanks! You can now download me to your phone.";
+  } else {
+    return `Oops, it seems like I can't reach out to ${number}`;
+  }
+}
+
+/**
+ * Given a certain response from the user, help the chatbot get only the URL.
+ *
+ * @param {string} userInput
+ * @returns {string[] | null} all the possible URL's that the user may have answered
+ */
+export function getURL(userInput) {
+  let regex = /[a-z]+\.[a-z]+/g;
+  return userInput.match(regex);
+}
+
+/**
+ * Greet the user using the full name data from the profile.
+ *
+ * @param {string} fullName
+ * @returns {string} Greeting from the chatbot
+ */
+export function niceToMeetYou(fullName) {
+  let regex = /([A-Z][a-z]+), ([A-Z][a-z]+)/g;
+  return `Nice to meet you, ${fullName.replace(regex, "$2 $1")}`;
+}
