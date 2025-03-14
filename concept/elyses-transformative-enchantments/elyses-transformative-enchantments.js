257983d33ed29992cf129dd82eef7e6a68830204
Drahiri <kodzidg@o2.pl>
2025-03-14 12:05:00 +0100

    Array Transformations

---
+++ concept/elyses-transformative-enchantments/elyses-transformative-enchantments.js
@@ -0,0 +1,95 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((value) => value * 2);
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  return deck.reduce((result, card) => {
+    if (card === 3) {
+      // Ignoring "Argument of type '3' is not assignable to parameter of type 'never'."
+      // @ts-ignore
+      result.push(3, 3, 3);
+    } else {
+      // @ts-ignore
+      result.push(card);
+    }
+    return result;
+  }, []);
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  deck.splice(0, deck.length / 2 - 1);
+  deck.splice(2, deck.length - 2);
+  return deck;
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  const bottomCard = deck.splice(0, 1)[0];
+  const topCard = deck.splice(deck.length - 1, 1)[0];
+  deck.splice(deck.length / 2, 0, topCard, bottomCard);
+  return deck;
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  return deck.filter((value) => value === 2);
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  return deck.sort((a, b) => a - b);
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse();
+}
